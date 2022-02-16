62baf75042c0932eadbb49873be15f202530803d
NagyPatrik024 <nagy.patrik024@gmail.com>
2022-02-16 13:29:29 +0100

    Commit

---
+++ QuizApp/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _2ora
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<Question> questions = new List<Question>();
+            questions.Add(new Question("Mi a neve Dél-Korea legnagyobb technológiai vállalatának?", new string[] { "Samsung", "Xiaomi", "LG", "Sony" }, "Samsung"));
+            questions.Add(new Question("Hány lélegzetet vesz az emberi test naponta?", new string[] { "20000", "200000", "1500", "1000000" }, "20000"));
+            questions.Add(new Question("Melyik évben készült az első Tonka teherautó?", new string[] { "1947", "1949", "1950", "1945" }, "1947"));
+
+            questions.ForEach(t =>
+            {
+                Label l = new Label();
+                l.Tag = t;
+                l.Background = Brushes.LightBlue;
+                l.Margin = new Thickness(20);
+                l.Width = this.ActualWidth / 6;
+                l.Height = this.ActualHeight / 6;
+                wrap.Children.Add(l);
+
+                l.MouseLeftButtonDown += wrap_MouseLeftButtonDown;
+            });
+        }
+
+        private void wrap_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Label l = (sender as Label);
+            Question q = (Question)(l.Tag);
+
+            QuestionWindow qw = new QuestionWindow(q);
+            if (qw.ShowDialog() == true)
+            {
+                l.Background = Brushes.LightGreen;
+                l.IsEnabled = false;
+            }
+            else
+            {
+                l.Background = Brushes.LightPink;
+                l.IsEnabled = false;
+            }
+        }
+    }
+
+    public class Question
+    {
+        private string question;
+        private string[] answers;
+        private string correct;
+
+
+        public Question(string question, string[] answers, string correct)
+        {
+            this.question = question;
+            this.answers = answers;
+            this.correct = correct;
+        }
+
+        public string QuestionProp { get => question; set => question = value; }
+        public string[] Answers { get => answers; set => answers = value; }
+        public string Correct { get => correct; set => correct = value; }
+    }
+}
---
+++ QuizApp/QuestionWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace _2ora
+{
+    public partial class QuestionWindow : Window
+    {
+        private Random rnd;
+        private Question q;
+        private DispatcherTimer timer;
+        private TimeSpan time;
+
+        public QuestionWindow(Question q)
+        {
+            InitializeComponent();
+            this.q = q;
+            rnd = new Random();
+            timer = new DispatcherTimer();
+            time = TimeSpan.FromSeconds(60);
+
+            timer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, delegate
+            {
+                l_timer.Content = time.ToString("ss");
+                if (time == TimeSpan.Zero && this.DialogResult == null)
+                    this.DialogResult = false;
+                time = time.Add(TimeSpan.FromSeconds(-1));
+            }, Application.Current.Dispatcher);
+
+            timer.Start();
+        }
+
+        private void b_Click(object sender, RoutedEventArgs e)
+        {
+            if ((sender as Button).Content == q.Correct)
+            {
+                this.DialogResult = true;
+            }
+            else
+            {
+                this.DialogResult = false;
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (this.DialogResult == null)
+            {
+                MessageBoxResult result = MessageBox.Show("Biztos ki akarsz lépni?", "Kilépés", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                if (result == MessageBoxResult.Yes)
+                {
+                    this.DialogResult = false;
+                }
+                else
+                {
+                    e.Cancel = true;
+                }
+            }
+
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            int n = q.Answers.Length;
+            while (n > 1)
+            {
+                int k = rnd.Next(n--);
+                string temp = q.Answers[n];
+                q.Answers[n] = q.Answers[k];
+                q.Answers[k] = temp;
+            }
+
+            int i = 0;
+
+            l_question.Content = q.QuestionProp;
+            foreach (var item in sp.Children)
+            {
+                if (item is Button)
+                {
+                    (item as Button).Content = q.Answers[i];
+                    i++;
+                }
+            }
+        }
+    }
+}
